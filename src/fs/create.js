003cb5078ca2ae645218add5b78a3d65bdd74ae9
Habrhammer <iamsorry1885@gmail.com>
2025-04-28 16:26:38 +0200

    feat: implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { writeFile } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(dirname(fileURLToPath(import.meta.url)), '/files/fresh.txt');
+
+  writeFile(filePath, 'I am fresh and young', { flag: 'wx' }, (err) => {
+    if (err?.code === 'EEXIST') {
+      console.error('FS operation failed');
+    }
+  });
 };
 
 await create();
