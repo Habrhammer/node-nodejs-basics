ea2d2f1f2fcfdceb21d0c20195cb2c8340ae5837
Habrhammer <iamsorry1885@gmail.com>
2025-04-28 23:17:42 +0200

    feat: implement function read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const readStream = createReadStream(`${__dirname}/files/fileToRead.txt`, {
+    encoding: "utf8",
+  });
+
+  readStream.pipe(process.stdout);
+
+  readStream.on("error", (err) => {
+    console.error(err.message);
+  });
 };
 
 await read();
