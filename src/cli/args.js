6f406758749febbee954eb973833e7de609164e8
Habrhammer <iamsorry1885@gmail.com>
2025-04-29 01:32:42 +0200

    feat: implement parseEnv

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
-const parseArgs = () => {
-    // Write your code here 
+const parseEnv = () => {
+  const args = process.argv.slice(2);
+  const result = args.reduce((acc, curr, index, array) => {
+    if (index % 2 === 0) {
+      let key = curr.startsWith("--") ? curr.slice(2) : curr;
+      const value = array[index + 1] || "";
+      acc.push(`RSS_${key}=${value}`);
+    }
+    return acc;
+  }, []);
+
+  const output = result.join("; ");
+  console.log(output);
 };
 
-parseArgs();
+parseEnv();
