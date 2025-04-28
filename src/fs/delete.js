2ae8557df2f1333a81f7206d740215b1ebc1ccee
Habrhammer <iamsorry1885@gmail.com>
2025-04-28 19:38:48 +0200

    feat: implement function remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { join } from 'node:path';
+import { unlink } from 'node:fs';
+
+
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(dirname(fileURLToPath(import.meta.url)), '/files/fileToRemove.txt');
+
+  unlink(filePath, (err) => {
+    if (err) {
+      console.error('FS operation failed');
+      return;
+    }
+    console.log('File removed successfully!');
+
+  });
 };
 
 await remove();
