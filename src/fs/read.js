3498f4cc766dfa2795ee7e9aeba9df849ada2e0d
Habrhammer <iamsorry1885@gmail.com>
2025-04-28 19:59:25 +0200

    feat: implement function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+ 
+  const filePath = join(dirname(fileURLToPath(import.meta.url)), '/files/fileToRead.txt');
+
+  readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('FS operation failed');
+      return;
+    };
+    console.log(data);
+  });
 };
 
 await read();
